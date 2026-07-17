71fbf4134e226a628b13e4a930ec2b3f7cdc4f57
ALagunskaya <nastya.emotion@gmail.com>
2026-07-17 14:48:44 +0500

    add fixture login_page

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,16 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from pages.text_box_page import TextBoxPage
-
-BASE_URL = "https://qa-guru.github.io/one-page-form/text-box.html"
+from pages.login_form_page import LoginPage
 
 
 @pytest.fixture
 def driver():
+    """Фикстура для браузера"""
     options = Options()
     options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
-    driver.get(BASE_URL)
     driver.implicitly_wait(5)
     yield driver
     driver.quit()
@@ -21,4 +20,14 @@ def driver():
 @pytest.fixture
 def text_box_page(driver):
     """Фикстура для страницы TextBox"""
-    return TextBoxPage(driver)
+    page = TextBoxPage(driver)
+    page.open()
+    return page
+
+
+@pytest.fixture
+def login_page(driver):
+    """Фикстура для страницы Login"""
+    page = LoginPage(driver)
+    page.open()
+    return page
